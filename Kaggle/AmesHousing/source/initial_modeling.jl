58dd9fe0a517680dc208e6cc5b4d39a45abce62c
MasonOgden <msogden@calpoly.edu>
2021-06-26 23:16:56 -0700

    fixed a tiny thing

--- Kaggle/AmesHousing/source/initial_modeling.jl
+++ Kaggle/AmesHousing/source/initial_modeling.jl
@@ -7,10 +7,8 @@ using Chain: @chain
 
 #%% Reading and Cleaning Data
 
-dtype_info(dataframe) = DataFrame(
-    col = names(dataframe),
-    dtype = eltype.(eachcol(dataframe))
-)
+dtype_info(dataframe) =
+    DataFrame(col = names(dataframe), dtype = eltype.(eachcol(dataframe)))
 
 has_value(value) = ismissing(value) ? 0.0 : 1.0
 
@@ -19,8 +17,8 @@ parse_int(value) = ismissing(value) ? missing : parse(Int64, value)
 # convert "NA" string values to missings
 function fix_na(input_vec)
     input_type = eltype(input_vec)
-    vector = convert(Vector{Union{Missing, input_type}}, input_vec)
-    vector[vector .== "NA"] .= missing
+    vector = convert(Vector{Union{Missing,input_type}}, input_vec)
+    vector[vector.=="NA"] .= missing
     return vector
 end
 
@@ -32,59 +30,61 @@ end
 function fix_dtypes(dataset)
     should_be_numeric = [:MasVnrArea, :LotFrontage, :GarageYrBlt]
     should_be_factor = [:MSSubClass, :MoSold, :YrSold]
-    
+
     out_df = @chain dataset begin
         # replace "NA" String values with missing
         mapcols(fix_na, _)
-        select(Symbol.(names(dataset)),
+        select(
+            Symbol.(names(dataset)),
             # convert string cols that should be numeric
             should_be_numeric .=> (x -> parse_int.(x)) .=> should_be_numeric,
             # convert int cols that should be categorical
-            should_be_factor .=> (x -> string.(x)) .=> should_be_factor
+            should_be_factor .=> (x -> string.(x)) .=> should_be_factor,
         )
     end
 
     # get names of all categorical columns
     cat_vars = @chain out_df begin
         dtype_info
-        filter(x -> x.dtype ∈ [String, Union{Missing, String}], _)
+        filter(x -> x.dtype ∈ [String, Union{Missing,String}], _)
         _[!, :col]
         Symbol.(_)
     end
 
     # convert all String columns to categorical
     @chain out_df begin
-        select(Symbol.(names(out_df)),
-            cat_vars .=> categorical .=> cat_vars
-            )
+        select(Symbol.(names(out_df)), cat_vars .=> categorical .=> cat_vars)
     end
 end
 
 # dichotomizes the 5 features with the most missing values
 function dichotomize_some_features(dataset)
     @chain dataset begin
-        select(Not([:PoolQC, :MiscFeature, :Alley, :Fence]),
+        select(
+            Not([:PoolQC, :MiscFeature, :Alley, :Fence]),
             :PoolQC => (x -> has_value.(x)) => :HasPool,
             :MiscFeature => (x -> has_value.(x)) => :HasMiscFeature,
             :Alley => (x -> has_value.(x)) => :HasAlley,
-            :Fence => (x -> has_value.(x)) => :HasFence
+            :Fence => (x -> has_value.(x)) => :HasFence,
         )
     end
 end
 
 function fix_lot_frontage(dataset)
     @chain dataset begin
-        select(Symbol.(names(dataset)),
-        # replace missings with 0.0
-        :LotFrontage => (x -> coalesce.(x, 0)) => :LotFrontage
+        select(
+            Symbol.(names(dataset)),
+            # replace missings with 0.0
+            :LotFrontage => (x -> coalesce.(x, 0)) => :LotFrontage,
         )
     end
 end
 
 function remove_missing_union(col_vector)
-    if (eltype(col_vector) == Union{Missing, CategoricalValue{String, UInt32}}) && sum(ismissing.(col_vector)) == 0
+    if (eltype(col_vector) == Union{Missing,CategoricalValue{String,UInt32}}) &&
+       sum(ismissing.(col_vector)) == 0
         convert(CategoricalArray, string.(col_vector))
-    elseif eltype(col_vector) == Union{Missing, Int64} && sum(ismissing.(col_vector)) == 0
+    elseif eltype(col_vector) == Union{Missing,Int64} && sum(ismissing.(col_vector)) == 0
         convert.(Int64, col_vector)
     else
         col_vector
@@ -95,15 +95,15 @@ function finalize_types(dataset)
     all_cols = Symbol.(names(dataset))
 
     @chain dataset begin
-        select(
-            all_cols .=> remove_missing_union .=> all_cols
-        )
+        select(all_cols .=> remove_missing_union .=> all_cols)
     end
 end
 
 
 #clean_data = set_factor_levels ∘ finalize_types ∘ fix_lot_frontage ∘ dichotomize_some_features ∘ fix_dtypes ∘ drop_unnecessary_features
-clean_data = finalize_types ∘ fix_lot_frontage ∘ dichotomize_some_features ∘ fix_dtypes ∘ drop_unnecessary_features
+clean_data =
+    finalize_types ∘ fix_lot_frontage ∘ dichotomize_some_features ∘ fix_dtypes ∘
+    drop_unnecessary_features
 
 dataset_dir = "datasets"
 
@@ -116,21 +116,17 @@ train |> schema
 
 function preprocess(dataset)
     #ordered_cat_vars = [:LotShape, :LandSlope, :BsmtExposure, :Functional,
-                        #:ExterQual, :ExterCond, :BsmtQual, :BsmtCond,
-                        #:HeatingQC, :KitchenQual, :GarageQual, :GarageCond]
+    #:ExterQual, :ExterCond, :BsmtQual, :BsmtCond,
+    #:HeatingQC, :KitchenQual, :GarageQual, :GarageCond]
 
     dataset_ready = @chain dataset srs finalize_types
 
-    cat_encoder = @chain OneHotEncoder(
-        ordered_factor = false, drop_last = true
-        ) begin
-            machine(dataset_ready)
-            MLJ.fit!
+    cat_encoder = @chain OneHotEncoder(ordered_factor = false, drop_last = true) begin
+        machine(dataset_ready)
+        MLJ.fit!
     end
 
-    standardizer = @chain Standardizer(
-        count = true
-        ) begin
+    standardizer = @chain Standardizer(count = true) begin
         machine(dataset_ready)
         MLJ.fit!
     end
@@ -140,23 +136,6 @@ function preprocess(dataset)
         MLJ.transform(cat_encoder, _)
     end
 end
-
-#%% Baseline Modeling Results
-
-# reg_pipelines = [
-#     (@load ARDRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load AdaBoostRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load BaggingRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load DecisionTreeRegressor pkg = DecisionTree verbosity = 0)(),
-#     (@load ElasticNetRegressor pkg = MLJLinearModels verbosity = 0)(),
-#     (@load EpsilonSVR pkg = LIBSVM verbosity = 0)(),
-#     (@load ExtraTreesRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load GradientBoostingRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load KNeighborsRegressor pkg = ScikitLearn verbosity = 0)(),
-#     (@load RandomForestRegressor pkg = DecisionTree verbosity = 0)(),
-#     (@load RidgeRegressor pkg = MLJLinearModels verbosity = 0)(),
-#     (@load SVMRegressor pkg = ScikitLearn verbosity = 0)()
-# ]
 
 reg_model_names = [
     "ARD",
@@ -170,131 +149,134 @@ reg_model_names = [
     "KNN",
     "Random Forest",
     "Ridge",
-    "SVM"
+    "SVM",
 ]
 
-ready_data(dataframe) = @chain dataframe srs finalize_types coerce(:SalePrice => Continuous) match_factor_levels(train)
+ready_data(dataframe) =
+    @chain dataframe srs finalize_types coerce(:SalePrice => Continuous) match_factor_levels(
+        train,
+    )
 
 reg_model_pipelines = [
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load ARDRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load ARDRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load AdaBoostRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load AdaBoostRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load BaggingRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load BaggingRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load DecisionTreeRegressor pkg = DecisionTree verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load DecisionTreeRegressor pkg = DecisionTree verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load ElasticNetRegressor pkg = MLJLinearModels verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load ElasticNetRegressor pkg = MLJLinearModels verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load EpsilonSVR pkg = LIBSVM verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load EpsilonSVR pkg = LIBSVM verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load ExtraTreesRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load ExtraTreesRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load GradientBoostingRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load GradientBoostingRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load KNeighborsRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load KNeighborsRegressor pkg = ScikitLearn verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load RandomForestRegressor pkg = DecisionTree verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load RandomForestRegressor pkg = DecisionTree verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load RidgeRegressor pkg = MLJLinearModels verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load RidgeRegressor pkg = MLJLinearModels verbosity = 0)()
     ),
     @pipeline(
-            # get data ready for modeling
-            X -> ready_data(X),
-            # Standardize numeric variables
-            Standardizer(count = true),
-            # Dummify categorical variables
-            OneHotEncoder(ordered_factor = false, drop_last = true),
-            # load into model
-            (@load SVMRegressor pkg = ScikitLearn verbosity = 0)()
+        # get data ready for modeling
+        X -> ready_data(X),
+        # Standardize numeric variables
+        Standardizer(count = true),
+        # Dummify categorical variables
+        OneHotEncoder(ordered_factor = false, drop_last = true),
+        # load into model
+        (@load SVMRegressor pkg = ScikitLearn verbosity = 0)()
     )
 ]
 
@@ -304,7 +286,14 @@ function match_factor_levels(dataset, full_data)
     # first, get names of categorical variables (minus BsmtCond and MSSubClass since those are special cases)
     cat_vars = @chain dataset begin
         dtype_info
-        filter(x -> x.dtype ∈ [CategoricalValue{String, UInt32}, Union{Missing, CategoricalValue{String, UInt32}}] && x.col ∉ ["BsmtCond", "MSSubClass"], _)
+        filter(
+            x ->
+                x.dtype ∈ [
+                    CategoricalValue{String,UInt32},
+                    Union{Missing,CategoricalValue{String,UInt32}},
+                ] && x.col ∉ ["BsmtCond", "MSSubClass"],
+            _,
+        )
         _[!, :col]
         Symbol.(_)
     end
@@ -314,7 +303,30 @@ function match_factor_levels(dataset, full_data)
         select(
             Symbol.(names(dataset)),
             :BsmtCond => (x -> levels!(x, ["Po", "Fa", "TA", "Gd", "Ex"])) => :BsmtCond,
-            :MSSubClass => (x -> levels!(x, ["20", "30", "40", "45", "50", "60", "70", "75", "80", "85", "90", "120", "150", "160", "180", "190"])) => :MSSubClass
+            :MSSubClass =>
+                (
+                    x -> levels!(
+                        x,
+                        [
+                            "20",
+                            "30",
+                            "40",
+                            "45",
+                            "50",
+                            "60",
+                            "70",
+                            "75",
+                            "80",
+                            "85",
+                            "90",
+                            "120",
+                            "150",
+                            "160",
+                            "180",
+                            "190",
+                        ],
+                    )
+                ) => :MSSubClass,
         )
     end
 
@@ -330,29 +342,36 @@ X_train = @chain train select(Not(:SalePrice))
 
 y_train = float.(train[!, :SalePrice])
 
+#%% Getting Baseline Results
+
 baseline_results = DataFrame(
     model = reg_model_names,
     cv_rmse = [
-        evaluate(reg, X_train, y_train, resampling=CV(shuffle=true, nfolds=5), verbosity=0).measurement[1]
-        for reg ∈ reg_model_pipelines
-    ]
+        evaluate(
+            reg,
+            X_train,
+            y_train,
+            resampling = CV(shuffle = true, nfolds = 5),
+            verbosity = 0,
+        ).measurement[1] for reg ∈ reg_model_pipelines
+    ],
 )
 
 baseline_results = @chain baseline_results sort(_, :cv_rmse)
 
 @df baseline_results bar(
-	:model,
-	:cv_rmse,
-	orientation = :h,
-	yflip = true,
-	legend = false,
-	xlabel = "Cross-Validated RMSE",
-	title = "Comparing Baseline Models"
+    :model,
+    :cv_rmse,
+    orientation = :h,
+    yflip = true,
+    legend = false,
+    xlabel = "Cross-Validated RMSE",
+    title = "Comparing Baseline Models",
 )
 
 # models I will tune: 
-    # 1. GradientBoostingRegressor
-    # 2. ExtraTrees
-    # 3. BaggingRegressor
-    # 4. RandomForestRegressor
-    # 5. ARD
+# 1. GradientBoostingRegressor
+# 2. ExtraTrees
+# 3. BaggingRegressor
+# 4. RandomForestRegressor
+# 5. ARD
